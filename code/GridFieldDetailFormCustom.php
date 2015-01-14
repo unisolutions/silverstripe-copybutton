75caf08c38c3739991ac0363fbbbbc90d938250f
Shea Dawson <shea@livesource.co.nz>
2015-01-14 17:08:21 +1100

    add error message to GridFieldDetailFormCustom.php advising about the changes

---
+++ code/GridFieldDetailFormCustom.php
@@ -0,0 +1,7 @@
+<?php
+
+class GridFieldDetailFormCustom extends GridFieldDetailForm{
+	public function __construct(){
+		user_error("The Component GridFieldDetailFormCustom is no longer required for GridFieldCopyButton. Please use GridFieldDetailForm ", E_USER_ERROR);
+	}
+}

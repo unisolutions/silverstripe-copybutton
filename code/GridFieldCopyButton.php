584300726fa1c1e9bc6f2006f905b51786a6d064
helpfulrobot <chris@silverstripe.com>
2015-12-23 22:15:49 +1300

    Converted to PSR-2

--- code/GridFieldCopyButton.php
+++ code/GridFieldCopyButton.php
@@ -8,57 +8,69 @@
  * @author Elvinas Liutkevičius <elvinas@unisolutions.eu>
  * @license BSD http://silverstripe.org/BSD-license
  */
-class GridFieldCopyButton implements GridField_ColumnProvider, GridField_ActionProvider {
+class GridFieldCopyButton implements GridField_ColumnProvider, GridField_ActionProvider
+{
 
-	public function augmentColumns($gridField, &$columns) {
-		if(!in_array('Actions', $columns))
-			$columns[] = 'Actions';
-	}
+    public function augmentColumns($gridField, &$columns)
+    {
+        if (!in_array('Actions', $columns)) {
+            $columns[] = 'Actions';
+        }
+    }
 
-	public function getColumnAttributes($gridField, $record, $columnName) {
-		return array('class' => 'col-buttons');
-	}
+    public function getColumnAttributes($gridField, $record, $columnName)
+    {
+        return array('class' => 'col-buttons');
+    }
 
-	public function getColumnMetadata($gridField, $columnName) {
-		if($columnName == 'Actions') {
-			return array('title' => '');
-		}
-	}
+    public function getColumnMetadata($gridField, $columnName)
+    {
+        if ($columnName == 'Actions') {
+            return array('title' => '');
+        }
+    }
 
-	public function getColumnsHandled($gridField) {
-		return array('Actions');
-	}
+    public function getColumnsHandled($gridField)
+    {
+        return array('Actions');
+    }
 
-	public function getActions($gridField) {
-		return array('copyrecord');
-	}
+    public function getActions($gridField)
+    {
+        return array('copyrecord');
+    }
 
-	public function getColumnContent($gridField, $record, $columnName) {
-		if(!$record->canCreate()) return;
-		$field = GridField_FormAction::create($gridField,  'CopyRecord'.$record->ID, false, "copyrecord",
-				array('RecordID' => $record->ID))
-			->addExtraClass('gridfield-button-copy')
-			->setAttribute('title', _t('GridAction.Copy', "Copy"))
-			->setDescription(_t('GridAction.COPY_DESCRIPTION','Copy'));
+    public function getColumnContent($gridField, $record, $columnName)
+    {
+        if (!$record->canCreate()) {
+            return;
+        }
+        $field = GridField_FormAction::create($gridField,  'CopyRecord'.$record->ID, false, "copyrecord",
+                array('RecordID' => $record->ID))
+            ->addExtraClass('gridfield-button-copy')
+            ->setAttribute('title', _t('GridAction.Copy', "Copy"))
+            ->setDescription(_t('GridAction.COPY_DESCRIPTION', 'Copy'));
 
-		return $field->Field();
-	}
+        return $field->Field();
+    }
 
-	public function handleAction(GridField $gridField, $actionName, $arguments, $data) {
-		if($actionName == 'copyrecord'){
-			$item = $gridField->getList()->byID($arguments['RecordID']);
-			if(!$item) return;
+    public function handleAction(GridField $gridField, $actionName, $arguments, $data)
+    {
+        if ($actionName == 'copyrecord') {
+            $item = $gridField->getList()->byID($arguments['RecordID']);
+            if (!$item) {
+                return;
+            }
 
-			if(!$item->canCreate()) {
-				throw new ValidationException(
-					_t('GridFieldAction_Copy.CreatePermissionsFailure',"No create permissions"),0);
-			}
+            if (!$item->canCreate()) {
+                throw new ValidationException(
+                    _t('GridFieldAction_Copy.CreatePermissionsFailure', "No create permissions"), 0);
+            }
 
-			$clone = $item->duplicate();
-			if (!$clone || $clone->ID < 1) {
-				user_error("Error Duplicating!", E_USER_ERROR);
-			}
-		}
-	}
-
+            $clone = $item->duplicate();
+            if (!$clone || $clone->ID < 1) {
+                user_error("Error Duplicating!", E_USER_ERROR);
+            }
+        }
+    }
 }
--- code/GridFieldDetailFormCustom.php
+++ code/GridFieldDetailFormCustom.php
@@ -1,7 +1,9 @@
 <?php
 
-class GridFieldDetailFormCustom extends GridFieldDetailForm{
-	public function __construct(){
-		user_error("The Component GridFieldDetailFormCustom is no longer required for GridFieldCopyButton. Please use GridFieldDetailForm ", E_USER_ERROR);
-	}
+class GridFieldDetailFormCustom extends GridFieldDetailForm
+{
+    public function __construct()
+    {
+        user_error("The Component GridFieldDetailFormCustom is no longer required for GridFieldCopyButton. Please use GridFieldDetailForm ", E_USER_ERROR);
+    }
 }
